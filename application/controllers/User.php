f43d28dc2a755d98430a6f180de7a3844ff5df87
YovitoAudie <yovito_audie@yahoo.com>
2018-05-13 23:31:03 +0700

    versi 1.0.1

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,16 +1,47 @@
- <?php 
+<?php
 
-class User extends CI_controller{
-	public function form(){
-		$this->load->view('form_user_v'); 
-	}
-	public function add(){
-		//get data
-		echo $this->input->post("username"). "<br>";
-		echo $this->input->post("password"). "<br>";
-		echo $this->input->post("fullname"). "<br>";
-		echo $this->input->post("level"). "<br>";
-	}
-}
+	/**
+	*  
+	*/
+	class User extends CI_Controller
+	{
+		//auto load model
+		function __construct(){
+			parent::__construct();
+			//load model
+			$this->load->model("user_m");
+		}
+		public function form(){
+			$this->load->view('form_user_v');
+		} 
+		function index(){
+			$this->load->model('user_m');
 
+			$data['user'] =$this->user_m->gets();
+
+			$this->load->view('user_data', $data);
+		}
+		public function add(){
+			//create data array
+			$data = array(
+				"username"=>$this->input->post("username"),
+				"password"=>$this->input->post("fullname"),
+				"fullname"=>$this->input->post("fullname"),
+				"level"=>$this->input->post("level")
+		);
+			function del($id){
+				$this->load->model("user_m");
+
+				$this->user_m->del($id);
+
+				redirect('user');
+			}
+			function edit ($id){}
+			function detail($id){}
+			//check data sebelum save
+			var_dump($data);
+			//save data to databse
+			$this->user_m->add($data);
+		}
+	}
 ?>
---
+++ application/models/user_m.php
@@ -0,0 +1,20 @@
+<?php
+
+class User_m extends CI_Model{
+	function gets(){
+		return $this->db->get('user')->result();
+
+
+	}
+	function get($id){}
+	/**
+		$data param data array=>column, value
+	*/
+	function add($data){
+		$this->db->insert("user" ,$data);
+	}
+	function edit($d){}
+	function del($id){
+		$this->db->delete('users', ["id"=>$id]);
+	}
+}
